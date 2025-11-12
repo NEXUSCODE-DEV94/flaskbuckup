f039fde01ffbadb35b08f70bfeeb9c2b7a76bf71
NEXUSCODE-DEV94 <nam636743@gmail.com>
2025-11-12 17:35:21 +0900

    Initialize environment variables in path.py

---
+++ path.py
@@ -0,0 +1,9 @@
+import os
+
+BOTTOKEN = os.getenv("BOTTOKEN")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
